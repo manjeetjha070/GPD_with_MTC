ec08f19ac0b8a1730bd848335072d2d266884569
manjeet <manjeet@campus.uni-paderborn.de>
2026-05-13 23:10:16 +0200

    set the directory of config file for gpd

--- gpd_ros/launch/ur5.launch.py
+++ gpd_ros/launch/ur5.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
                 'cloud_topic': '/camera/depth/color/points',
 
                 # Filepath to the configuration file for GPD
-                'config_file': '/home/pg/rrs_ss25/manjeet/ros/src/kinova_gen3_arm/gpd_ros/config/ros_eigen_params.cfg',
+                'config_file': '/home/pg/rrs_ss25/manjeet/ros/src/GPD_with_MTC/gpd_ros/config/ros_eigen_params.cfg',
 
                 # RViz topic to publish grasps
                 'rviz_topic': 'plot_grasps',
